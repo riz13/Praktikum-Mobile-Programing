ebc7d593dd9a80c6ee4eaf22ef46b82df41165a6
riz13 <56906181+riz13@users.noreply.github.com>
2021-03-26 13:15:10 +0700

    Add files via upload

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -46,68 +49,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        body: Container(
+          padding: const EdgeInsets.fromLTRB(16.0, 64.0, 16.0, 0.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  new Padding(
+                      padding: EdgeInsets.all(4.0),
+                      child: new Container(
+                        child: Image.asset ('HI.png'),
+                        width: 200.0,
+                        height: 150.0,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(16.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xD769A047),
+                              offset: Offset(0.0, 4.0),
+                              blurRadius: 12.0
+                            )
+                          ]
+                        ),
+                      padding: EdgeInsets.all(12.0),
+                      ),
+                  ),
+
+                  new Expanded(
+                      child: new Container(
+                        margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                        child: new Column(
+                          children: [
+                            new Text("Honkai Impact", textAlign: TextAlign.center, style: TextStyle(
+                                fontSize: 28.0,
+                              fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            new Padding(padding: EdgeInsets.all(2.0)),
+                            new Text("Play on PC"),
+                          ],
+                        ),
+                      )
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
